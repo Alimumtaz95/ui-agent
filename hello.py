a244b4a7324d23cadd63f64752ec48d72b313e51
Mumtaz Ali <mumtazalipintto@gmail.com>
2025-04-29 00:00:42 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ from agents.run import RunConfig
 load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
-# gemini_api_key = "AIzaSyCC_0HMvFgtYtxZrw-5iJcey1oMVTPYa_A"
+
 # Check if the API key is present; if not, raise an error
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
